19051f1dd7e377eb4811626d3466244f638f2151
Maksim Lin <maks@manichord.com>
2013-04-29 22:24:46 +1000

    fix to use renamed 'add' param

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = index;
 var walk = require('git-walk-tree')
 
 /**
- * write - callback function to store entries in the index, it will be called for each entry to 
+ * add - callback function to store entries in the index, it will be called for each entry to 
  *         be written into the git index. It receives a single argument a object with properties:
  *           path{String}, hash{Buffer}, type {Number}, mode, lastmod {Date}
  *  
@@ -23,6 +23,6 @@ function index(commit, find, add) {
           }
           idxEntry.path += data.stack.map(function(s) { return s.name}).join("/")
           idxEntry.mode = (data.stack.length > 0) ? data.stack[data.stack.length-1].mode : 0;
-          write(idxEntry)
+          add(idxEntry)
       })
 }
